3c6ae1a3020be317d1ff134e75b0e5b5c98197b8
Ramin Karimpor <ramin.kp81@gmail.com>
2024-02-02 15:07:24 +0330

    developing postForm component

--- src/components/templates/PostForm.jsx
+++ src/components/templates/PostForm.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-import { Button, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { useQuery } from "@tanstack/react-query";
 
 import { getCategories } from "../../services/admin";
+import customToast from "./../../utils/toast";
+import axios from "axios";
+import { getCookie } from "../../utils/cookie";
 
 function PostForm() {
-  const [formData, setFormData] = useState({
+  const [form, setForm] = useState({
     title: "",
     content: "",
     amount: "",
@@ -13,16 +26,81 @@ function PostForm() {
     category: "",
     images: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
   const { data } = useQuery(["get-category"], getCategories);
 
   const changeHandler = (event) => {
     const name = event.target.name;
-    console.log(name);
+    if (name !== "images") {
+      setForm({
+        ...form,
+        [name]: event.target.value,
+      });
+    } else {
+      const validate = event?.target.files[0].type.split("/")[0] !== "image";
+      if (validate) {
+        customToast("error", "لطفا عکس محصول را وارد کنید");
+        return;
+      }
+      setForm({
+        ...form,
+        [name]: event.target.files[0],
+      });
+    }
+  };
+  useEffect(() => {
+    setForm({
+      title: "",
+      content: "",
+      amount: "",
+      city: "",
+      category: "",
+      images: "",
+    });
+  }, [isLoading]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (
+      !form.amount ||
+      !form.category ||
+      !form.city ||
+      !form.content ||
+      !form.images ||
+      !form.title
+    ) {
+      customToast("error", "تمام فیلد هارو پر کنید");
+      return;
+    }
+    setIsLoading(true);
+    const token = getCookie("accessToken");
+    const formData = new FormData();
+
+    for (let i in form) {
+      formData.append(i, form[i]);
+    }
+    axios
+      .post(`${import.meta.env.VITE_BASE_URL}post/create`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        customToast("success", "محصول جدید با موفقیت ثبت شد");
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        customToast("مشکلی پیش آمده است");
+        setIsLoading(false);
+      });
   };
 
   return (
     <form
       onChange={changeHandler}
+      onSubmit={submitHandler}
       style={{
         display: "flex",
         flexDirection: "column",
@@ -46,6 +124,7 @@ function PostForm() {
         variant="outlined"
         color="mainColor"
         name="title"
+        value={form.title}
       />
       <TextField
         id="outline-basic"
@@ -55,6 +134,7 @@ function PostForm() {
         rows={4}
         color="mainColor"
         name="content"
+        value={form.content}
       />
       <TextField
         id="outline-basic"
@@ -63,6 +143,7 @@ function PostForm() {
         color="mainColor"
         type="number"
         name="amount"
+        value={form.amount}
       />
       <TextField
         id="outline-basic"
@@ -70,8 +151,9 @@ function PostForm() {
         variant="outlined"
         color="mainColor"
         name="city"
+        value={form.city}
       />
-      {/* <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
         <InputLabel id="demo-simple-select-autowidth-label" color="mainColor">
           دسته بندی
         </InputLabel>
@@ -81,8 +163,10 @@ function PostForm() {
           label="دسته بندی "
           color="mainColor"
           size="medium"
-          sx={{ maxWidth: "150px" }}
           name="category"
+          value={form.category}
+          onChange={changeHandler}
+          sx={{ maxWidth: "150px" }}
         >
           {data?.data.slice(0, 6).map((item) => (
             <MenuItem key={item._id} value={item._id}>
@@ -90,31 +174,27 @@ function PostForm() {
             </MenuItem>
           ))}
         </Select>
-      </FormControl> */}
-
-      <select style={{ maxWidth: "150px" }} name="category">
-        {data?.data.slice(0, 6).map((item) => (
-          <option key={item._id} value={item._id}>
-            {item.name}
-          </option>
-        ))}
-      </select>
+      </FormControl>
       <Button
         variant="contained"
         component="label"
+        endIcon={<CloudUploadIcon />}
+        color="mainColor"
         sx={{ fontSize: "1rem", fontWeight: "600", width: "30%" }}
       >
         آپلود عکس محصول
         <input type="file" name="images" hidden />
       </Button>
-      <Button
+      <LoadingButton
         variant="contained"
-        component="label"
+        type="submit"
         color="mainColor"
+        loading={isLoading}
+        disabled={isLoading}
         sx={{ fontSize: "1rem", fontWeight: "600", width: "30%" }}
       >
-        ثبت محصول
-      </Button>
+        <span>ثبت محصول</span>
+      </LoadingButton>
     </form>
   );
 }
