f85536ab83374e4aff1fe9d6c2ab7dc4ee59ae94
Ramin Karimpor <ramin.kp81@gmail.com>
2024-02-02 23:44:51 +0330

    styling CheckOtpForm

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -2,6 +2,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { postPhoneNumber } from "../../services/auth";
 import customToast from "../../utils/toast";
+import { Button, TextField, Typography } from "@mui/material";
 
 function CheckOtpForm({ mobile, setMobile, setStep }) {
   const submitHandler = async (event) => {
@@ -14,20 +15,48 @@ function CheckOtpForm({ mobile, setMobile, setStep }) {
     }
   };
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <h1>ورود به حساب کاربری</h1>
-        <input
-          type="text"
-          placeholder="شماره موبایل  خود را وارد"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "calc(100vh - 210px)",
+      }}
+    >
+      <form
+        onSubmit={submitHandler}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "400px",
+          height: "400px",
+          padding: "20px",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          boxShadow: "rgba(0,0,0,0.5) 0 0 5px",
+        }}
+      >
+        <Typography variant="h5" color="#991b1b" fontWeight="600">
+          ورود به حساب کاربری
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          label="شماره موبایل"
+          variant="outlined"
+          color="mainColor"
           value={mobile}
           onChange={(e) => {
             setMobile(e.target.value);
           }}
         />
-        <button type="submit">تأیید</button>
+        <Button variant="contained" color="mainColor" type="submit">
+          تأیید
+        </Button>
       </form>
-    </>
+    </div>
   );
 }
 
